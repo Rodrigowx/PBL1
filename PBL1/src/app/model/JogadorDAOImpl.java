737fc2079bc05871641c7008fd622790b257f0f4
josen <josen@DESKTOP-0QVLME2>
2022-09-23 14:52:12 -0300

    Arrumou o scan

--- PBL1/src/app/model/JogadorDAOImpl.java
+++ PBL1/src/app/model/JogadorDAOImpl.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class JogadorDAOImpl implements JogadorDAO{
 	
+	Scanner scan = new Scanner(System.in); 
 	Map<String, Jogador> mapJogadores = new HashMap<String, Jogador>(); //Lista para armazenar os objetos Jogador
 
 	@Override
@@ -23,36 +24,38 @@ public class JogadorDAOImpl implements JogadorDAO{
 		
 		if (mapJogadores.containsKey(idJogador)) { 
 			
+			
+			
 			//Condicional para a opção que o usuário colocou no menu na main
 			
 			if (opcaoMenu == 1) { //EDITA NOME
 				
-				Scanner scan = new Scanner(System.in);
+				
 				String novoNome = scan.nextLine();
 				mapJogadores.get(idJogador).setNome(novoNome);//FAZER VERIFICAÇÃO DE ERRO DE TODOS
 				
 				
 			}else if (opcaoMenu == 2) { //EDITA POSIÇÃO
 				
-				Scanner scan = new Scanner(System.in);
+				
 				String novaPosic = scan.nextLine();
 				mapJogadores.get(idJogador).setPosicao(novaPosic);
 				
 			}else if (opcaoMenu == 3) { //EDITA CARTÕES AMARELO
 				
-				Scanner scan = new Scanner(System.in);
+				
 				Integer novoCartA = scan.nextInt();
 				mapJogadores.get(idJogador).setCartAmarelo(novoCartA);
 				
 			}else if (opcaoMenu == 4) { //EDITA CARTÕES VERMELHO
 				
-				Scanner scan = new Scanner(System.in);
+				
 				Integer novoCartV = scan.nextInt();
 				mapJogadores.get(idJogador).setCartVermelho(novoCartV);
 				
 			}else if (opcaoMenu == 5) { //EDITA QUANTIDADE DE GOLS
 				
-				Scanner scan = new Scanner(System.in);
+				
 				Integer novoGols = scan.nextInt();
 				mapJogadores.get(idJogador).setGols(novoGols);
 				

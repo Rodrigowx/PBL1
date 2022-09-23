52fb3edbc642d0561cce5af765b32e7803375222
josen <josen@DESKTOP-0QVLME2>
2022-09-23 14:38:50 -0300

    Implementação da classe e DAO da Seleção

--- PBL1/src/app/model/Selecao.java
+++ PBL1/src/app/model/Selecao.java
@@ -1,5 +1,50 @@
 package app.model;
 
-public class Selecao {
+import java.util.*;
 
+public class Selecao {
+	
+	//ATRIBUTOS
+	private String nome;
+	private List<Jogador> jogadores;
+	private Tecnico tecnico;
+	
+	
+	//CONSTRUTOR
+	public Selecao(String nome, List<Jogador> jogadores, Tecnico tecnico) {
+		super();
+		this.nome = nome;
+		this.jogadores = jogadores;
+		this.tecnico = tecnico;
+	}
+
+	//METODOS
+	
+	public String toString() {
+		return this.nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Jogador> getJogadores() {
+		return jogadores;//AQUI PRECISARIA LISTAR CADA UM DA LISTA
+	}
+
+	public void setJogadores(List<Jogador> jogadores) {
+		this.jogadores = jogadores;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
 }
--- PBL1/src/app/model/SelecaoDAOImpl.java
+++ PBL1/src/app/model/SelecaoDAOImpl.java
@@ -1,5 +1,55 @@
 package app.model;
 
-public class SelecaoDAOImpl {
+import java.util.*;
+
+public class SelecaoDAOImpl implements SelecaoDAO {
+
+	ArrayList<Selecao> listaSelecoes = new ArrayList<>(); 
+	
+	@Override
+	public boolean inserir(Selecao selecao) {
+		
+		listaSelecoes.add(selecao); //**
+		
+		return true;
+	}
+
+	@Override
+	public boolean editar(String nomeSelecao, String novoNome) {
+		
+		for (int i = 0; i < listaSelecoes.size(); i++) {
+			
+			if (listaSelecoes.get(i).getNome() == nomeSelecao) {
+				
+				listaSelecoes.get(i).setNome(novoNome);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public Selecao excluir(String nomeSelecao) {
+		
+		/*explicar aqui o que faz*/
+		
+		for (int i = 0; i < listaSelecoes.size(); i++) {
+			
+			if (listaSelecoes.get(i).getNome() == nomeSelecao) {
+				
+				Selecao removed = listaSelecoes.get(i);
+				listaSelecoes.remove(i);
+				return removed;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void listar() {
+		for (int i = 0; i < listaSelecoes.size(); i++) {
+			System.out.println(listaSelecoes.get(i).getNome());
+		}
+	}
 
 }
